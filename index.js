bdc41e67b51729e3c23dac54374f7a1bb00bde55
Endel Dreyer <endel.dreyer@gmail.com>
2014-09-05 15:53:20 -0300

    fix file reference

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs'),
     esprima = require('esprima-fb');
 
-var exampleFile = fs.readFileSync('examples/advogados.js').toString();
+var exampleFile = fs.readFileSync('examples/klass_example.js').toString();
 var ast = esprima.parse(exampleFile);
 console.log(JSON.stringify(ast, null, 4));
