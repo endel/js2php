740e18d15f019bbb4e53fa6b58fb130893279097
Endel Dreyer <endel.dreyer@gmail.com>
2014-09-05 17:22:27 -0300

    refactoring

--- index.js
+++ index.js
@@ -11,32 +11,28 @@ function visit(node, level) {
 
   level = (typeof(level)==='undefined') ? 0 : level;
 
-  var tmp = "";
-
   if (node.type == "VariableDeclaration") {
     for (var i=0;i<node.declarations.length;i++) {
-      tmp += visit(node.declarations[i]);
+      content += visit(node.declarations[i]);
     }
 
   } else if (node.type == "VariableDeclarator") {
-    tmp = '$' + node.id.name + ' = ' + visit(node.init) + ";\n"
+    content = '$' + node.id.name + ' = ' + visit(node.init) + ";\n"
 
   } else if (node.type == "Identifier") {
-    tmp = "$" + node.name;
+    content = "$" + node.name;
 
   } else if (node.type == "Literal") {
-    tmp = node.raw;
+    content = node.raw;
 
   } else if (node.type == "BinaryExpression") {
-    tmp = visit(node.left) + " " + node.operator + " " + visit(node.right);
+    content = visit(node.left) + " " + node.operator + " " + visit(node.right);
 
   } else if (node.type == "Program") {
     for (var i=0;i<node.body.length;i++) {
-      tmp += visit(node.body[i]);
+      content += visit(node.body[i]);
     }
   }
-
-  content += tmp;
 
   return content;
 }
