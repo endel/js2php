921ba6b2c1707cee44cb07562324dcf60be9e53e
Endel Dreyer <endel.dreyer@gmail.com>
2014-09-18 20:45:23 -0300

    bump version 0.0.5

--- examples/closures.js
+++ examples/closures.js
@@ -1,9 +1,10 @@
+d = 10;
 var closure = function(a) {
   var c = 1;
   return function(b) {
-    return a + b + c;
+    return a + b + c + d;
   }
 }
 
 calling = closure(5);
-var_dump(calling(4) == 10);
+var_dump(calling(4) == 20);
