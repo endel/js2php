be7b0786d6df8e5f12e1449d365c0413d6943cff
Endel Dreyer <endel.dreyer@gmail.com>
2015-03-03 23:22:10 -0300

    use espree instead of esprima-fb

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 var core = require('./core'),
     scope = require('./scope'),
     utils = require('./utils'),
-    esprima = require('esprima-fb');
+    espree = require('espree');
 
 module.exports = function(code) {
-  var ast = esprima.parse(code, {
+  var ast = espree.parse(code, {
     loc : true,
     range : true,
     tokens : true,
     comment : true,
+    ecmaFeatures: {
+      arrowFunctions: true, // enable parsing of arrow functions
+      blockBindings: true, // enable parsing of let/const
+      destructuring: true, // enable parsing of destructured arrays and objects
+      regexYFlag: true, // enable parsing of regular expression y flag
+      regexUFlag: true, // enable parsing of regular expression u flag
+      templateStrings: true, // enable parsing of template strings
+      binaryLiterals: true, // enable parsing of binary literals
+      octalLiterals: true, // enable parsing of ES6 octal literals
+      unicodeCodePointEscapes: true, // enable parsing unicode code point escape sequences
+      defaultParams: true, // enable parsing of default parameters
+      restParams: true, // enable parsing of rest parameters
+      forOf: true, // enable parsing of for-of statement
+      objectLiteralComputedProperties: true, // enable parsing computed object literal properties
+      objectLiteralShorthandMethods: true, // enable parsing of shorthand object literal methods
+      objectLiteralShorthandProperties: true, // enable parsing of shorthand object literal properties
+      objectLiteralDuplicateProperties: true, // Allow duplicate object literal properties (except '__proto__')
+      generators: true, // enable parsing of generators/yield
+      spread: true, // enable parsing spread operator
+      classes: true, // enable parsing classes
+      globalReturn: true // enable return in global scope
+    }
   });
 
   var rootScope = scope.create(ast, scope.KIND_ROOT);
@@ -277,7 +299,7 @@ module.exports = function(code) {
       }
 
     } else if (node.type == "ClassDeclaration") {
-      content = "class " + node.id.name
+      content = "class " + node.name.name
 
       if (node.superClass) {
         content += " extends " + node.superClass.name;
