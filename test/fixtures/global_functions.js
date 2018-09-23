b59ba3cad7d43a88db34b87558b5d11bd11ec93f
Chris Brown <chris@2pha.com>
2018-09-23 16:19:31 +1000

    global functions scope fix.

--- test/fixtures/global_functions.js
+++ test/fixtures/global_functions.js
@@ -3,3 +3,7 @@ var float = parseFloat("50.50");
 
 var_dump(integer);
 var_dump(float);
+
+var temp = '50.50';
+var_dump(parseInt(temp));
+var_dump(parseFloat(temp));
--- test/fixtures/global_functions.php
+++ test/fixtures/global_functions.php
@@ -3,3 +3,6 @@ $integer = intval("50");
 $float = floatval("50.50");
 var_dump($integer);
 var_dump($float);
+$temp = '50.50';
+var_dump(intval($temp));
+var_dump(floatval($temp));
